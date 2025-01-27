c0064937f77bec459606299d5c5b94e0e6dee9dc
tiago-coderia <asimplekreative@gmail.com>
2025-01-27 12:10:39 -0300

    Mudado link para clicar e copiar

--- pages/components/Contact/index.js
+++ pages/components/Contact/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import Button from '../Buttons/primary';
+import { useState } from 'react';
 
 const Contact = () => {
+  const [buttonText, setButtonText] = useState('Entrar em contato');
+  const email = 'tiiago.dsg@gmail.com';
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        setButtonText('Email Copiado!'); // Atualiza o texto do botão
+        setTimeout(() => setButtonText('Entrar em contato'), 2000); // Restaura o texto após 2 segundos
+      })
+      .catch((err) => {
+        console.error('Erro ao copiar texto:', err);
+      });
+  };
   return (
     <div className="text-center flex flex-col items-center gap-4 border-b-[1px] pb-10 border-default  border-opacity-10">
       <h1 className="text-gradient text-5xl font-semibold leading-normal text-center">
@@ -9,9 +24,9 @@ const Contact = () => {
         Seja visto, seja lembrado!
       </h1>
       <span className="text-4xl animate-pulse">😀</span>
-      <Button endereco="mailto:asimplekreative@gmail.com">
-        <div className="flex items-center gap-4">
-          Entrar em contato
+      <Button>
+        <div className="flex items-center gap-4" onClick={copyToClipboard}>
+          {buttonText}
           <ion-icon name="mail-unread-outline"></ion-icon>
         </div>
       </Button>
--- pages/components/Header/index.js
+++ pages/components/Header/index.js
@@ -1,20 +1,35 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [buttonText, setButtonText] = useState('Vamos conversar?');
+  const email = 'tiiago.dsg@gmail.com';
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        setButtonText('Email Copiado!'); // Atualiza o texto do botão
+        setTimeout(() => setButtonText('Vamos conversar?'), 2000); // Restaura o texto após 2 segundos
+      })
+      .catch((err) => {
+        console.error('Erro ao copiar texto:', err);
+      });
+  };
+
   return (
-    <div className="min-h-[100px] flex items-center justify-between border-b-[1px] border-default  border-opacity-10 flex-col sm:flex-row py-4 sm:py-0">
+    <div className="min-h-[100px] flex items-center justify-between border-b-[1px] border-default border-opacity-10 flex-col sm:flex-row py-4 sm:py-0">
       <div>
-        <Link
-          href="mailto:asimplekreative@gmail.com"
+        <button
+          onClick={copyToClipboard}
           className="link-primary flex items-center gap-4"
           target="_blank"
         >
           <div className="iconHeader">
             <ion-icon name="mail-outline" />
           </div>
-          <p className="">Vamos conversar?</p>
-        </Link>
+          <p>{buttonText}</p>
+        </button>
       </div>
       <div className="flex gap-10">
         <Link
@@ -45,4 +60,5 @@ const Header = () => {
     </div>
   );
 };
+
 export default Header;
